435e78939369585e105b84a15599041d20dbe2f2
aan1805 <aan1805@gmail.com>
2019-07-12 16:49:57 +0300

    fix browser

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.ArrayList;
@@ -18,50 +19,50 @@ public class MainClass {
         //инициализация
         System.setProperty("webdriver.gecko.driver", "/home/alex/IdeaProjects/TestSelenium/drivers/geckodriver");
         System.setProperty("webdriver.chrome.driver", "/home/alex/IdeaProjects/TestSelenium/drivers/chromedriver");
-        WebDriver driver = new FirefoxDriver();
-        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS); //implicit timeout
-        driver.manage().window().maximize();
+        WebDriver browser = new FirefoxDriver();
+        browser.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS); //implicit timeout
+        browser.manage().window().maximize();
 
-        driver.get("http://" + sitePageYan);
+        browser.get("http://" + sitePageYan);
 
         //radioButton checkBox
-        driver.findElement(By.xpath("/html/body/div[1]/div/span/div[2]/noindex/div[2]/div/div/div/div[3]/a")).click();
-        driver.findElement(By.xpath("/html/body/div[1]/div[2]/div[7]/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/ul/li[1]/div/a")).click();
-        driver.findElement(By.xpath("/html/body/div[1]/div[5]/div[2]/div[2]/div/div/div/div[3]/div/div[2]/div[3]/fieldset/ul/li[1]/div/a/label/div")).click();
+        browser.findElement(By.xpath("/html/body/div[1]/div/span/div[2]/noindex/div[2]/div/div/div/div[3]/a")).click();
+        browser.findElement(By.xpath("/html/body/div[1]/div[2]/div[7]/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/ul/li[1]/div/a")).click();
+        browser.findElement(By.xpath("/html/body/div[1]/div[5]/div[2]/div[2]/div/div/div/div[3]/div/div[2]/div[3]/fieldset/ul/li[1]/div/a/label/div")).click();
 
-/*
+
         //ссылки
-        WebElement link = driver.findElement(By.xpath("//li[@id='n-aboutsite']/a"));
+        WebElement link = browser.findElement(By.xpath("//li[@id='n-aboutsite']/a"));
         System.out.println(link.getText());
         link.click();
 
 
         //работа с текстовыми полями
-        driver.findElement(By.xpath("//input[@id='searchInput']")).sendKeys("Selenium WebDriver");
-        driver.findElement(By.xpath("//form[@id='searchform']//input[last()]")).click();
-        System.out.println(driver.findElement(By.xpath("//div[@id='searchText']//input")).getAttribute("value"));
-        driver.findElement(By.xpath("//div[@id='searchText']//input")).clear();
+        browser.findElement(By.xpath("//input[@id='searchInput']")).sendKeys("Selenium WebDriver");
+        browser.findElement(By.xpath("//form[@id='searchform']//input[last()]")).click();
+        System.out.println(browser.findElement(By.xpath("//div[@id='searchText']//input")).getAttribute("value"));
+        browser.findElement(By.xpath("//div[@id='searchText']//input")).clear();
 
 
         //поиск элементов
-        WebElement link = driver.findElement(By.linkText("Log in"));
-        WebElement searchField = driver.findElement(By.name("search"));
-        WebElement searchButton = driver.findElement(By.xpath("//form[@id='searchform']//input[@id='searchButton']"));
-        WebElement li = driver.findElement(By.id("ca-viewsource"));
+        WebElement linkLog = browser.findElement(By.linkText("Log in"));
+        WebElement searchField = browser.findElement(By.name("search"));
+        WebElement searchButton = browser.findElement(By.xpath("//form[@id='searchform']//input[@id='searchButton']"));
+        WebElement li = browser.findElement(By.id("ca-viewsource"));
 
 
-
+/*
         //формирование массива данных
         WebElement[] data = {
                 link,
                 searchField,
                 searchButton};
-
+*/
 
         //формирование листа данных
         List<WebElement> data = new ArrayList<>();
 
-        data.add(link);
+        data.add(linkLog);
         data.add(searchField);
         data.add(searchButton);
         data.add(li);
@@ -73,8 +74,8 @@ public class MainClass {
         }
 
         searchButton.click();
-        */
-       // driver.quit();
+
+        browser.quit();
 
     }
 
